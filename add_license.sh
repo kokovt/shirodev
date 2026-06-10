5cfa07c92cc465f47dfc40447d9e9c3be429b565
Shiro <shirodev@noreply.codeberg.org>
2026-06-09 23:55:11 -0400

    Easter egg, modular header, and custom pages

--- add_license.sh
+++ add_license.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 # 1. Create a template file with the dynamic year and holder
-echo "Copyright (c) $(date +%Y) Shiro.
+echo "shirodev.dev - my personal website
+Copyright (c) $(date +%Y) Shiro.
+
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version." > temp-agpl.txt
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>." > temp-agpl.txt
 
 # 2. Run addlicense using this temporary file
 addlicense -f temp-agpl.txt .
